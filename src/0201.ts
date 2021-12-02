5363d916d973db64ec6f574937a800acaa43158b
PupoSDC <pedro.pupo@greator.com>
2021-12-02 08:27:19 +0000

    style: apply styleguide

--- src/0201.ts
+++ src/0201.ts
@@ -7,29 +7,32 @@ enum Command {
 }
 
 type Position = {
-  depth: number,
+  depth: number;
   horizontal: number;
 };
 
 const position = fs
   .readFileSync("./data/0200.txt", "utf8")
   .split("\n")
-  .reduce<Position>((position, entry) => {
-    const split = entry.split(" ");
-    const command = split[0] as Command;
-    const value = Number(split[1]);
-    switch (command) {
-      case Command.DOWN:
-        position.depth += value;
-        break;
-      case Command.UP:
-        position.depth -= value;
-        break;
-      case Command.FORWARD:
-        position.horizontal += value;
-        break;
-    }
-    return position;
-  }, { depth: 0, horizontal: 0});
+  .reduce<Position>(
+    (position, entry) => {
+      const split = entry.split(" ");
+      const command = split[0] as Command;
+      const value = Number(split[1]);
+      switch (command) {
+        case Command.DOWN:
+          position.depth += value;
+          break;
+        case Command.UP:
+          position.depth -= value;
+          break;
+        case Command.FORWARD:
+          position.horizontal += value;
+          break;
+      }
+      return position;
+    },
+    { depth: 0, horizontal: 0 }
+  );
 
 console.log(position.depth * position.horizontal);
